dd69f7ae6ec4633c0990fd2674407826cf806d58
ljb525 <61051870+ljb525@users.noreply.github.com>
2022-06-02 10:23:43 +0800

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,9 +1,9 @@
-#!/usr/bin/env python
+#!/usr/bin/env find me 
 import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "test1.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "find-me.settings")
 
     from django.core.management import execute_from_command_line
 
